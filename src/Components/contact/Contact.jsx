a562c4df926ae13cb062f0dc07902409f1c29e06
Caiote <silvacaio295@gmail.com>
2022-03-09 21:19:34 -0300

    finalizando

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -4,19 +4,19 @@ import { MdOutlineEmail } from "react-icons/md";
 import { RiMessengerLine } from "react-icons/ri"
 import {BsWhatsapp} from "react-icons/bs"
 import { useRef } from 'react';
-// import emailjs from 'emailjs-com'
+import emailjs from 'emailjs-com'
 
 
 const Contact = () => {
   const form = useRef()
 
-  // const sendEmail = (e) => {
-  //   e.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-  //   emailjs.sendForm('service_hex44yf', 'template_1my7y5c', form.current, 'CQOi8-0ChE4ALBWiY')
+    emailjs.sendForm('service_hex44yf', 'template_1my7y5c', form.current, 'CQOi8-0ChE4ALBWiY')
 
-  //   e.target.reset()
-  // }; 
+    e.target.reset()
+  }; 
 
   return (
     <section id="contact"> 
@@ -29,26 +29,21 @@ const Contact = () => {
          <MdOutlineEmail className="contact__option-icon"/>
          <h4>Email</h4>
          <h5>silvacaio295@gmail.com</h5>
-         <a href="mailto:silvacaio295@gmail.com" target='_blank' rel="noreferrer">Send a message</a>
+         <a href="mailto:silvacaio295@gmail.com" target='_blank' className="btn">Send a message</a>
        </article>
 
-       <article className="contact__option">
-         <RiMessengerLine className="contact__option-icon"/>
-         <h4>Messenger</h4>
-         <h5>silvacaio295@gmail.com</h5>
-         <a href="https://m.me/ernest.achiever" target='_blank' rel="noreferrer">Send a message</a>
-       </article>
+       
 
        <article className="contact__option">
          <BsWhatsapp className="contact__option-icon"/>
          <h4>WhatsApp</h4>
-         <h5>silvacaio295@gmail.com</h5>
-         <a href="https://api.whatsapp.com/send?phone=+559499033173" target='_blank' rel="noreferrer">Send a message</a>
+         <h5>My Number</h5>
+         <a href="https://api.whatsapp.com/send?phone=+559499033173" target='_blank' className="btn">Send a message</a>
        </article>
 
       </div>
       {/* EN OF CONTACT OPTIONS */}
-      <form ref={form} /*onSubmit={sendEmail}*/> 
+      <form ref={form} onSubmit={sendEmail}> 
         <input type="text" name="name" placeholder="Your Full Name required" />
         <input type="email" name="email" placeholder="Your Email" required/>
         <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
